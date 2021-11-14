fd8127581810031eebe42fe9b9bd7f5bb4d8dfee
ningpop <craft1933@naver.com>
2021-11-15 01:45:06 +0900

    ➕[ADD] : group model

--- introduction/models.py
+++ introduction/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
+from group.models import Group
+
 # Create your models here.
 
 class Introduction(models.Model):
     #user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    #group_code = models.ForeignKey(Group, on_delete=models.CASCADE)
+    group_code = models.ForeignKey(Group, on_delete=models.CASCADE)
     #question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
     answer = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
