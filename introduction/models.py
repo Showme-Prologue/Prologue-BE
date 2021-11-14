dd45aff5bfbec1e23ef902478ca1d60dde16b099
ningpop <craft1933@naver.com>
2021-11-15 01:37:43 +0900

    [ADD] introduction model

--- introduction/models.py
+++ introduction/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Introduction(models.Model):
+    #user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    #group_code = models.ForeignKey(Group, on_delete=models.CASCADE)
+    #question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.answer[:100]
